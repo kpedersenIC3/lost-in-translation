d612030ee41dcae346f2748c2c2ad7952b775f5f
Kasper Pedersen <kasper.dharma@gmail.com>
2022-02-01 15:45:57 +0100

    feat:added user context

--- src/App.js
+++ src/App.js
@@ -3,18 +3,30 @@ import './App.css';
 import Translate from './components/Views/Translate'
 import StartPage from './components/Views/StartPage'
 import ProfilePage from './components/Views/ProfilePage'
+import { createContext } from 'react';
+
+//create contexts
+export const LoggedIn = createContext()
+export const UserAPI = createContext()
+
+//make a default user to be updated on login
+let currentUser = {id:0,username:"",translations:[],loggedIn:false}
+
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Routes>
-          <Route path='/' element={<StartPage />} />
-          <Route path='/translate' element={<Translate />} />
-          <Route path='/profile' element={<ProfilePage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+        <UserAPI.Provider value={currentUser}>
+        <BrowserRouter>
+          <div>
+            <Routes>
+              <Route path='/' element={<StartPage />} />
+              <Route path='/translate' element={<Translate />} />
+              <Route path='/profile' element={<ProfilePage />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+        </UserAPI.Provider> 
   );
 }
 
